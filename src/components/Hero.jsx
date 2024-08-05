b3cc4286e419b7aa49077c88430bab7cbe8339c9
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 16:53:47 +0530

    styled span and p

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,13 +3,16 @@ const Hero = () => {
     <div className="min-h-screen flex flex-col gap-10 items-center justify-center">
       <p>IT&apos;S TIME TO GET</p>
       <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-        Swole <span>normous</span>
+        Swole <span className="text-blue-400">normous</span>
       </h1>
-      <p>
+      <p className="text-sm md:text-base font-light">
         I hereby acknowledgement that I may become{" "}
-        <span>unbelievably swolenormous </span>and accept all risks of becoming
-        the local <span> mass monstrosity</span>, afflicted with severe body
-        dismorphia, unable to fit through doors.
+        <span className="text-blue-400 font-medium">
+          unbelievably swolenormous{" "}
+        </span>
+        and accept all risks of becoming the local{" "}
+        <span className="text-blue-400 font-medium"> mass monstrosity</span>,
+        afflicted with severe body dismorphia, unable to fit through doors.
       </p>
     </div>
   );
