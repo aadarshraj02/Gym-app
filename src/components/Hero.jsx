d3ffd50299d72f1fae48b76401a9a1207f4d17ba
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 15:04:24 +0530

    added a h1 and p for header

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,17 @@
 const Hero = () => {
-  return <div></div>;
+  return (
+    <div>
+      <p>IT&apos;S TIME TO GET</p>
+      <h1>
+        Swole <span>normous</span>
+      </h1>
+      <p>
+        I hereby acknowledgement that I may become unbelievably swolenormous and
+        accept all risks of becoming the local mass monstrosity, afflicted with
+        severe body dismorphia, unable to fit through doors.
+      </p>
+    </div>
+  );
 };
 
 export default Hero;
