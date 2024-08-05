61b6c66a7d8df940b91523dc74a3063fd86780f7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 17:54:44 +0530

    styled p of header

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,9 +1,9 @@
 const SectionWrapper = (props) => {
   const { children, header, title } = props;
   return (
-    <section className="min-h0screen flex flex-col gap-10">
+    <section className="min-h-screen flex flex-col gap-10">
       <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center">
-        <p>{header}</p>
+        <p className="font-medium uppercase">{header}</p>
         <p>
           {title[0]} <span> {title[1]} </span> {title[2]}
         </p>
