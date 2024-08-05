a3e6b901f6a970aecea33178ad17b52f0a0be5a7
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 14:53:58 +0530

    imported workout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Hero from "./components/Hero";
 import Generator from "./components/Generator";
+import Workout from "./components/Workout";
 
 const App = () => {
   return (
     <div>
       <Hero />
       <Generator />
+      <Workout />
     </div>
   );
 };
