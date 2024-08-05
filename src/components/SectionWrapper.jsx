142b197d26dfb1001bf53587f825b84808b3df08
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 17:56:37 +0530

    styled title and span

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -4,8 +4,10 @@ const SectionWrapper = (props) => {
     <section className="min-h-screen flex flex-col gap-10">
       <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center">
         <p className="font-medium uppercase">{header}</p>
-        <p>
-          {title[0]} <span> {title[1]} </span> {title[2]}
+        <p className="font-semibold text-3xl sm:text-4xl lg:text-6xl">
+          {title[0]}{" "}
+          <span className="text-blue-400 uppercase"> {title[1]} </span>{" "}
+          {title[2]}
         </p>
       </div>
       {children}
