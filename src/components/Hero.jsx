d80935972a91ff67cf3fb4c81350b958ba90d9be
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 17:04:15 +0530

    added button styled and added div width

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center">
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[900px] w-full mx-auto">
       <div className="flex flex-col gap-4">
         <p>IT&apos;S TIME TO GET</p>
         <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
@@ -16,6 +16,9 @@ const Hero = () => {
         <span className="text-blue-400 font-medium"> mass monstrosity</span>,
         afflicted with severe body dismorphia, unable to fit through doors.
       </p>
+      <button className="px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+        <p>Accept & Begin</p>
+      </button>
     </div>
   );
 };
