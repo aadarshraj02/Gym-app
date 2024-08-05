64840fd729649b1f8c754b64d6074155bdde60ee
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 18:09:33 +0530

    added function and passed index title and description

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -1,5 +1,18 @@
 import SectionWrapper from "./SectionWrapper";
 
+function Header(props) {
+  const { index, title, description } = props;
+  return (
+    <div>
+      <div>
+        <p>{index}</p>
+        <h4>{title}</h4>
+      </div>
+      <p>{description}</p>
+    </div>
+  );
+}
+
 const Generator = () => {
   return (
     <SectionWrapper
