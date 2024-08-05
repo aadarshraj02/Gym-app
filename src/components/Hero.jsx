d698c25d33843137aa04968c5a54efda8a19ea08
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 17:19:20 +0530

    added padding on main div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 const Hero = () => {
   return (
-    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[900px] w-full mx-auto">
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[900px] w-full mx-auto p-4">
       <div className="flex flex-col gap-4">
         <p>IT&apos;S TIME TO GET</p>
         <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
