dccad5906ca518a2cba89ccbc845676bdade86bf
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 17:36:06 +0530

    added section and div and made flex container

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -1,6 +1,10 @@
 const SectionWrapper = (props) => {
-  const { children } = props;
-  return <div>{children}</div>;
+  const { children, header, title } = props;
+  return (
+    <section className="min-h0screen flex flex-col gap-10">
+      <div className="bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center"></div>
+    </section>
+  );
 };
 
 export default SectionWrapper;
