07534ccbc40a3583fb446f5223e1dc6ff89a6423
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 14:56:51 +0530

    added class to main div

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,11 @@ import Workout from "./components/Workout";
 
 const App = () => {
   return (
-    <div>
+    <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
       <Generator />
       <Workout />
-    </div>
+    </main>
   );
 };
 
