3f0f33a75c5b70b96586e61139802ea949da5856
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 16:56:09 +0530

    wrapped in div

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 const Hero = () => {
   return (
     <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center">
-      <p>IT&apos;S TIME TO GET</p>
-      <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
-        Swole <span className="text-blue-400">normous</span>
-      </h1>
+      <div>
+        <p>IT&apos;S TIME TO GET</p>
+        <h1 className="uppercase font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          Swole <span className="text-blue-400">normous</span>
+        </h1>
+      </div>
       <p className="text-sm md:text-base font-light">
         I hereby acknowledgement that I may become{" "}
         <span className="text-blue-400 font-medium">
