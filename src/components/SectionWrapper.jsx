5da7b2f4c3475a657afee0a7d18eb6613d18a55f
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-05 17:51:17 +0530

    passed header title and children in p

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -2,7 +2,13 @@ const SectionWrapper = (props) => {
   const { children, header, title } = props;
   return (
     <section className="min-h0screen flex flex-col gap-10">
-      <div className="bg-slate-950 py-10 flex flex-col gap-4 justify-center items-center"></div>
+      <div className="bg-slate-950 py-10 flex flex-col gap-2 justify-center items-center">
+        <p>{header}</p>
+        <p>
+          {title[0]} <span> {title[1]} </span> {title[2]}
+        </p>
+      </div>
+      {children}
     </section>
   );
 };
