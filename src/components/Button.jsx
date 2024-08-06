5f6957dfc01a442d8adc0f7b72138c66b82e63b6
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-06 21:01:32 +0530

    added styling of button and passed text as props

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,10 @@
-const Button = () => {
-  return <div></div>;
+const Button = (props) => {
+  const { text } = props;
+  return (
+    <button className="px-8 mx-auto py-4 rounded-md border-[2px] bg-slate-950 border-blue-400 border-solid blueShadow duration-200">
+      <p>{text}</p>
+    </button>
+  );
 };
 
 export default Button;
