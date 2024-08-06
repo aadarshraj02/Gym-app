c99ed915eea4af0024f84f142ff95dabec6980d3
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-06 08:47:25 +0530

    styled div and p of index title and description

--- src/components/Generator.jsx
+++ src/components/Generator.jsx
@@ -3,12 +3,14 @@ import SectionWrapper from "./SectionWrapper";
 function Header(props) {
   const { index, title, description } = props;
   return (
-    <div>
-      <div>
-        <p>{index}</p>
-        <h4>{title}</h4>
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-center gap-2">
+        <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">
+          {index}
+        </p>
+        <h4 className="text-lg sm:text-2xl md:text-3xl">{title}</h4>
       </div>
-      <p>{description}</p>
+      <p className="text-sm sm:text-base mx-auto">{description}</p>
     </div>
   );
 }
