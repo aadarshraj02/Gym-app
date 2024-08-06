08d947f0b72c952750b697c428e3a9e73a511bf8
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-06 21:19:07 +0530

    added poison muscle and goals

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,21 @@ import { useState } from "react";
 
 const App = () => {
   const [workout, setWorkout] = useState(null);
+  const [poison, setPoison] = useState("individual");
+  const [muscle, setMuscle] = useState([]);
+  const [goals, setGoals] = useState("strength_power");
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
-      <Generator />
+      <Generator
+        poison={poison}
+        setPoison={setPoison}
+        muscles={muscle}
+        setMuscles={setMuscle}
+        goal={goals}
+        setGoal={setGoals}
+        updateWorkout={updateWorkout}
+      />
       {workout && <Workout workout={workout} />}
     </main>
   );
