6dc0910ea0c98ded9b04e77dbdd24a9bf1dce5a5
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-06 22:01:45 +0530

    added function update workout

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,23 @@ import Hero from "./components/Hero";
 import Generator from "./components/Generator";
 import Workout from "./components/Workout";
 import { useState } from "react";
+import { generateWorkout } from "./utils/functions";
 
 const App = () => {
   const [workout, setWorkout] = useState(null);
   const [poison, setPoison] = useState("individual");
   const [muscle, setMuscle] = useState([]);
   const [goals, setGoals] = useState("strength_power");
+
+  function updateWorkout() {
+    if (muscle.length < 1) {
+      return;
+    }
+    let newWorkout = generateWorkout({ poison, muscles, goal });
+    setWorkout(newWorkout);
+
+    window.location.href = "#workout";
+  }
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
@@ -18,6 +29,7 @@ const App = () => {
         setMuscle={setMuscle}
         goal={goals}
         setGoal={setGoals}
+        updateWorkout={updateWorkout}
       />
       {workout && <Workout workout={workout} />}
     </main>
