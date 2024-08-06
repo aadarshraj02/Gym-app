5d0594c69df5e697c48aaa0881de8654338f368e
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-06 21:14:11 +0530

    used state to set workout

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Hero from "./components/Hero";
 import Generator from "./components/Generator";
 import Workout from "./components/Workout";
+import { useState } from "react";
 
 const App = () => {
+  const [workout, setWorkout] = useState(null);
   return (
     <main className="min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-950 text-white text-sm sm:text-base">
       <Hero />
       <Generator />
-      <Workout />
+      {workout && <Workout workout={workout} />}
     </main>
   );
 };
