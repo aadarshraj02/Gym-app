327e0ffad5ce0982d35396e596abfe58f3388371
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-07 08:28:44 +0530

    added index in h4 styled div

--- src/components/ExerciseCard.jsx
+++ src/components/ExerciseCard.jsx
@@ -1,8 +1,16 @@
 import React from "react";
 
 const ExerciseCard = (props) => {
-    const {exercise} = props
-  return <div></div>;
+  const { exercise } = props;
+  return (
+    <div className="p-4 rounded-md flex flex-col gap-4 bg-slate-950 sm:flex-wrap">
+      <div className="flex flex-col sm:flex-row sm:items-center sm:flex-wrap gap-x-4 ">
+        <h4 className="text-3xl hidden sm:inline smL:text-4xl md:text-5xl font-semibold text-slate-400">
+          0{i + 1}
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default ExerciseCard;
