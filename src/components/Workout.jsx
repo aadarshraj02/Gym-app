06c5e2222bf3f74254a0bcbc05632ae87e9dfaf0
aadarshraj02 <aadarshraazz007@gmail.com>
2024-08-07 08:18:47 +0530

    added div and added exercise card

--- src/components/Workout.jsx
+++ src/components/Workout.jsx
@@ -3,11 +3,12 @@ import SectionWrapper from "./SectionWrapper";
 const Workout = (props) => {
   const { workout } = props;
   return (
-    <SectionWrapper
-      header={"Welcome to"}
-      title={["The", "Danger", "Zone"]}
-    >
-      {" "}
+    <SectionWrapper header={"Welcome to"} title={["The", "Danger", "Zone"]}>
+      <div className="flex flex-col gap-4">
+        {workout.map((exercise, i) => (
+          <ExerciseCard />
+        ))}
+      </div>
     </SectionWrapper>
   );
 };
